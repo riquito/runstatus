f509e5dc4b9a4a3c034e8c1d9c3917e8170e35d6
Riccardo Attilio Galli <riccardo@sideralis.org>
2011-09-15 02:50:38 +0200

    Initial import

---
+++ src/run_once/__init__.py
@@ -0,0 +1,24 @@
+
+# This file has mostly been done looking at the original PSF os.py
+
+# Note:  more names are added to __all__ later.
+__all__ = []
+
+import sys
+
+_names = sys.builtin_module_names
+
+def _get_exports_list(module):
+    try:
+        return list(module.__all__)
+    except AttributeError:
+        return [n for n in dir(module) if n[0] != '_']
+
+if 'posix' in _names:
+    from posix import *
+    import posix
+    __all__.extend(_get_exports_list(posix))
+    del posix
+
+elif 'nt' in _names:
+    pass
---
+++ src/run_once/posix.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+'''
+Copyright 2011 Riccardo Attilio Galli <riccardo@sideralis.org> [http://www.sideralis.org]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+import os,sys,atexit
+from fcntl import lockf,LOCK_EX,LOCK_NB,LOCK_UN
+from tempfile import gettempdir
+
+from time import mktime,strptime
+from datetime import date,datetime,timedelta
+
+def get_system_start_time():
+    today = date.today()
+    askTime,uptime = (x.strip() for x in os.popen('uptime').read().split(',')[0].split('up'))
+    tsUptime = strptime(uptime,'%H:%M')
+    tsAskTime = strptime(askTime,'%H:%M:%S')
+    now = datetime(today.year,today.month,today.day) + timedelta(hours=tsAskTime.tm_hour,minutes=tsAskTime.tm_min)
+    startTime = now - timedelta(hours=tsUptime.tm_hour,minutes=tsUptime.tm_min,seconds=tsUptime.tm_sec)
+    return mktime(startTime.utctimetuple())
+
+def process_exists(pid,partial_name=''):
+    """
+    Check if a process is running
+    
+    :param pid: process id
+    :type pid:  int
+    :param partial_name: process' name (or part of it)
+    :type partial_name:  string
+    
+    :return: whether the process is running
+    :rtype: bool
+    """
+    
+    pid = str(pid).strip()
+    if not pid: return false
+    pid = int(pid)
+    
+    process_string = os.popen('ps -p %d -o cmd=' % pid).read().strip()
+    return process_string and partial_name in process_string
+
+def process_kill(pid):
+    """
+    End the process identified by pid.
+    
+    :param pid: process id
+    :type pid:  int
+    """
+    try:
+      os.kill(int(pid),9) # send SIGKILL to pid
+    except (OSError,e):
+      if e.errno == 3: # No such process
+        pass
+      else: raise e
+
+def isSingleInstance(codeName):
+    """Discover if the program is yet running as another process"""
+    
+    pathLockFile = os.path.join(gettempdir(),'lock_'+codeName)
+    
+    fd=os.open(pathLockFile,os.O_RDWR|os.O_CREAT)
+    fp=os.fdopen(fd,'r+')
+    
+    isSingle = False
+    
+    try:
+        lockf(fp,LOCK_EX|LOCK_NB)
+        line = fp.read()
+        
+        pid = ''
+        if line:
+           pid, pidSysStartTime = (int(x) for x in line.split())
+
+        sysStartTime = int(get_system_start_time())
+        
+        isSingle = not (pid and sysStartTime == pidSysStartTime and process_exists(pid))
+        
+        if isSingle:
+            # app is not running, update the content of the lock file
+            fp.seek(0)
+            fp.write('%d %d' % (os.getpid(),sysStartTime))
+            fp.truncate()
+            atexit.register(os.remove,pathLockFile)
+        
+        lockf(fp,LOCK_UN)
+        fp.close()
+    
+    except IOError,e:
+        if e.errno == 11: # Resource temporarily unavailable 
+           pass           # (failed to lock because we're not alone)
+        else: raise e
+    finally:
+        try: fp.close()
+        except: pass
+    
+    return isSingle
+    
+
